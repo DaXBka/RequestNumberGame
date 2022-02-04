2396bc40ffa9a5abba79ac3ad509642f3249d564
DaHbka <flexinzflexinz@gmail.com>
2022-02-04 03:11:50 +0300

    Задание 1 выполнено

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+function game(requestNumber) {
+    alert("Угадай число от 1 до 100");
+
+    const gameQuestion = function () {
+        let userNumber = prompt("Введи число в поле");
+
+        if (isNumber(userNumber)) {
+            userNumber = +userNumber;
+
+            if (userNumber > requestNumber) {
+                alert("Загаданное число меньше");
+                gameQuestion();
+            } else if (userNumber < requestNumber) {
+                alert("Загаданное число больше");
+                gameQuestion();
+            } else if (userNumber == requestNumber) {
+                alert("Поздравляю, ты угадал!");
+            }
+        } else if (userNumber == null) {
+            alert("Игра окончена");
+        } else {
+            alert("Вводи только числа!");
+            gameQuestion();
+        }
+    };
+    gameQuestion();
+}
+
+game(10);
