30c4ddf7ef7b1d457452c1900516ee3cfa587004
DaHbka <flexinzflexinz@gmail.com>
2022-02-04 03:43:45 +0300

    HardUpgrade. Задание выполнено

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 "use strict";
 
-// Я вынес усложненное задание в отдельную ветку,
-// может быть, так удобнее будет?
-// Вторая ссылка на отдельную ветку.
-
-// Загаданное число вносится в конце файла
-// как аргумент для запуска функции игры.
+let requestNumberForGame = 10; // Число для игры
 
 const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
+    return !isNaN(parseFloat(num)) && isFinite(num); // Проверка числа, число ли оно?
 };
+
+game(requestNumberForGame); // Старт игры (function declaration)
 
 function game(requestNumber) {
     alert("Угадай число от 1 до 100");
+    let attempts = 10; // Кол-во попыток в замыкании
 
     const gameQuestion = function () {
-        let userNumber = prompt("Введи число в поле");
+        if (attempts > 0) {
 
-        if (isNumber(userNumber)) {
-            userNumber = +userNumber;
+            let userNumber = prompt("Введи число в поле.");
 
-            if (userNumber > requestNumber) {
-                alert("Загаданное число меньше");
+            if (isNumber(userNumber)) {
+                userNumber = +userNumber;
+
+                if (userNumber > requestNumber) {
+                    --attempts;
+                    alert(`Загаданное число меньше.\n\nОсталось попыток: ${attempts}`);
+                    gameQuestion();
+
+                } else if (userNumber < requestNumber) {
+                    --attempts;
+                    alert(`Загаданное число больше.\n\nОсталось попыток: ${attempts}`);
+                    gameQuestion();
+
+                } else if (userNumber == requestNumber) {
+                    if (confirm("Поздравляю, ты угадал! Хотите сыграть ещё?")) game(requestNumberForGame);
+                    else alert("Игра окончена. Спасибо!");
+                    // Положительный ответ полностью перезапускает игру.
+                    // Новое число можно определять через Math.random и сохранять,
+                    // пока всё на одном числе в переменной requestNumberForGame.
+                }
+
+            } else if (userNumber === null) {
+                alert("Игра окончена.");
+
+            } else {
+                alert("Вводи только числа!");
                 gameQuestion();
-            } else if (userNumber < requestNumber) {
-                alert("Загаданное число больше");
-                gameQuestion();
-            } else if (userNumber == requestNumber) {
-                alert("Поздравляю, ты угадал!");
             }
-        } else if (userNumber == null) {
-            alert("Игра окончена");
+
         } else {
-            alert("Вводи только числа!");
-            gameQuestion();
+            if (confirm("Ваши попытки закончились. Хотите сыграть ещё?")) game(requestNumberForGame);
+            else alert("Игра окончена. Спасибо!");
         }
     };
     gameQuestion();
 }
-
-game(10);
